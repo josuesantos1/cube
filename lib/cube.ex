763dadb60b89d1001c454c45af63b320eedc300a
josue santos <josue.santos.dev@gmail.com>
2025-10-03 14:21:40 -0300

    start project

---
+++ lib/cube.ex
@@ -0,0 +1,18 @@
+defmodule Cube do
+  @moduledoc """
+  Documentation for `Cube`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cube.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
