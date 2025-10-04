5dca3b3e677d963d51c78d9eac3da3c0eed8522c
josue santos <josue.santos.dev@gmail.com>
2025-10-04 00:49:42 -0300

    return query

--- lib/parser/data.ex
+++ lib/parser/data.ex
@@ -15,7 +15,11 @@ defmodule Parser.Data do
 
     IO.puts("Encoding data: key_length=#{key_length}, key_hex=#{key_hexadecimal}, type=#{type}, length_value=#{length_value}, value=#{value}")
 
-    key_length <> key_hexadecimal <> to_string(type) <> length_value <> to_string(value) <> "\n"
+    %Parser.Query{
+      command: key_length <> key_hexadecimal <> to_string(type) <> length_value <> to_string(value) <> "\n",
+      type: :set,
+      shard: get_shard(key_hexadecimal)
+    }
   end
 
   def encoding(%{command: :get, key: key}) do
@@ -30,7 +34,7 @@ defmodule Parser.Data do
       |> Integer.to_string(16)
       |> String.pad_leading(3, "0")
 
-    %Parser.Query{command: key_length <> key_hexadecimal}
+    %Parser.Query{command: key_length <> key_hexadecimal, type: :get, shard: get_shard(key_hexadecimal)}
   end
 
   def decoding_string(data_string) do
@@ -48,5 +52,12 @@ defmodule Parser.Data do
       value: value,
       type: type
     }
+  end
+
+  defp get_shard(id) do
+    :erlang.phash2(id, 20)
+    |> rem(20)
+    |> Integer.to_string()
+    |> String.pad_leading(2, "0")
   end
 end
