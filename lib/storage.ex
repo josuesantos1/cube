ae86fe7047b8f18c82bb7488dd5e468026c1eb92
josue santos <josue.santos.dev@gmail.com>
2025-10-04 01:09:56 -0300

    feat: get the data

--- lib/storage.ex
+++ lib/storage.ex
@@ -1,6 +1,7 @@
 defmodule Storage do
   def exec(%Parser.Query{type: :get, command: command, shard: shard}) do
     IO.puts("Executing query: #{command}")
+    get(command, shard)
   end
 
  # mudar para receber query
@@ -12,8 +13,26 @@ defmodule Storage do
     :ok
   end
 
-  def get(id) do
-    IO.puts("Retrieving data for ID: #{id}")
-    {:ok, %{id: id, data: "Sample Data"}}
+  defp get(command, shard) do
+    IO.puts("Retrieving data for command: #{command} in shard: #{shard}")
+
+    file_path = shard <> "_data.txt"
+
+    if File.exists?(file_path) do
+      file_path
+      |> File.stream!()
+      |> Enum.find(fn line ->
+        String.starts_with?(line, command)
+      end)
+      |> case do
+        nil -> "NIL"
+        line ->
+          line
+          |> String.trim()
+          |> Parser.Data.decoding_string()
+      end
+    else
+      "NIL"
+    end
   end
 end
