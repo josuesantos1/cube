e42bed45a11d280cc08b4752f2ced00114dba755
josue santos <josue.santos.dev@gmail.com>
2025-10-04 00:49:24 -0300

    use a new struct query to parten mathing

--- lib/storage.ex
+++ lib/storage.ex
@@ -1,29 +1,19 @@
 defmodule Storage do
-  def exec(%Parser.Query{command: command}) do
+  def exec(%Parser.Query{type: :get, command: command, shard: shard}) do
     IO.puts("Executing query: #{command}")
   end
 
  # mudar para receber query
  # mudar parser query para adicionar um field shard
-  def exec(data) do
-    IO.puts("Storing data: #{inspect(data)}")
-
-    id = :erlang.phash2(data, 20)
-    filename = get_shard(id)
-    |> Integer.to_string()
-    |> String.pad_leading(2, "0")
-    |> Kernel.<>("_data.txt")
-
-    File.write(filename, data, [:append])
+  def exec(%Parser.Query{type: :set, command: command, shard: shard}) do
+    IO.puts("Executing command: #{command}")
+    # adicionar value
+    File.write(shard <> "_data.txt", command, [:append])
     :ok
   end
 
   def get(id) do
     IO.puts("Retrieving data for ID: #{id}")
     {:ok, %{id: id, data: "Sample Data"}}
-  end
-
-  defp get_shard(id) do
-    rem(id, 20)
   end
 end
