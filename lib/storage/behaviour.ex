c4fd8a2cb79e53c78477b8b40b4c54f715eeeb0d
josue santos <josue.santos.dev@gmail.com>
2025-10-05 10:36:01 -0300

    remove modules legacies

--- lib/storage/behaviour.ex
+++
@@ -1,4 +0,0 @@
-defmodule Storage.Behaviour do
-  @callback get(key :: String.t()) :: {:ok, any()} | {:error, term()}
-  @callback set(key :: String.t(), value :: Parser.Value.t()) :: {:ok, any()} | {:already_exists, any()}
-end
